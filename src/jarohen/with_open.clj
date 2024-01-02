883532babd86fba81a2e8f6308c14dc8aac144a5
Alex Hall <534841+alexhall@users.noreply.github.com>
2024-01-02 15:29:35 -0500

    Port try-finally macro and use in with-open+

--- src/jarohen/with_open.clj
+++ src/jarohen/with_open.clj
@@ -5,6 +5,29 @@
 
 (set! *warn-on-reflection* true)
 
+(defmacro try-finally
+  "Use try/finally such that an exception in the finally clause does not
+  mask a prior exception in the try block."
+  [body _ finally-body]
+  `(let [*e# (atom nil)]
+     (try
+       ~body
+       (catch Throwable e#
+         (reset! *e# e#))
+       (finally
+         (try
+           ~finally-body
+           (catch Throwable e2#
+             ;; if an exception was thrown by the first try body and by the first finally block then avoid masking the
+             ;; first exception
+             (if @*e#
+               ;; add the new exception to the prior exception
+               (.addSuppressed ^Throwable @*e# e2#)
+               (throw e2#)))
+           (finally
+             (when @*e#
+               (throw @*e#))))))))
+
 (defprotocol Resource
   "Represents a resource with state that should be closed when no longer needed."
   (-close [resource]))
@@ -18,10 +41,10 @@
 
 (defn _with-open [resource f]
   (cond
-    (satisfies? Resource resource) (try
-                                     (f resource)
-                                     (finally
-                                       (-close resource)))
+    (satisfies? Resource resource) (try-finally
+                                    (f resource)
+                                    :finally
+                                    (-close resource))
 
     (fn? resource) (resource f)
     :else (throw (ex-info "Invalid resource passed to with-open+" {:resource resource}))))
