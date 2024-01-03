12947fb15f51ee3204efde71722c7ab4dd84fe45
Alex Hall <534841+alexhall@users.noreply.github.com>
2024-01-03 10:23:50 -0500

    Better docstrings

--- src/jarohen/with_open.clj
+++ src/jarohen/with_open.clj
@@ -7,7 +7,18 @@
 
 (defmacro try-finally
   "Use try/finally such that an exception in the finally clause does not
-  mask a prior exception in the try block."
+  mask a prior exception in the try block. The first arg is a form to be
+  evaluated in the main body of the `try`; the second arg is an unused marker
+  for readability (the suggested value is `:finally`); the third arg is a
+  form to be evaluated in the finally block of the `try`.
+
+  Example usage:
+  ```
+  (try-finally
+    (/ 1 0)
+    :finally
+    (println \"Cleaning up\"))
+  ```"
   [body _ finally-body]
   `(let [*e# (atom nil)]
      (try
@@ -49,7 +60,16 @@
     (fn? resource) (resource f)
     :else (throw (ex-info "Invalid resource passed to with-open+" {:resource resource}))))
 
-(defmacro with-open+ [bindings & body]
+(defmacro with-open+
+  "Like `clojure.core/with-open` this evaluates the body in a try expression with
+  the provided bindings that are cleaned up in a finally clause, but provides some
+  additional features:
+  - Bindings support destructuring forms and not just raw names.
+  - Arbitrary types can be treated as resources by wrapping them in higher-order
+    functions (or extending the Resource protocol).
+  - Exceptions thrown in the finally clause will not mask previous exceptions thrown
+    from the body (or inner finally clauses)."
+  [bindings & body]
   (if-let [[binding expr & more-bindings] (seq bindings)]
     `(_with-open ~expr (fn [~binding]
                          (with-open+ [~@more-bindings]
